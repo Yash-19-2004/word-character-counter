4c9d9bf64e1ce14d9036620f2813fa460b5ace14
MandeepJas <4nm20is077@nmamit.in>
2026-06-17 12:42:02 +0530

    Digital Heroes Word Counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+function App(){
+
+  const[text,setText] =useState("");
+
+  return(
+
+      <div className="container">
+    <h1>Word & Character Counter</h1>
+
+    <textarea
+    placeholder="Type here..."
+    value={text}
+    onChange={(e) => setText(e.target.value)}>
+    </textarea>
+
+    <h2>Characters: {text.length}</h2>
+
+    <h2>
+        Words:{" "}
+        {text.trim() === ""
+          ? 0
+          : text.trim().split(/\s+/).length}
+      </h2>
+
+      <div className="footer">
+        <h3>Yash Jaswal</h3>
+        <p>jaswalyash5@gmail.com</p>
+
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://digitalheroesco.com"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          <button>Built for Digital Heroes</button>
         </a>
-      </header>
-    </div>
+
+      </div>
+  </div>
   );
 }
 
